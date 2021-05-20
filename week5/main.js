77afeb7b62487f0a5b79c5c1b1c127d9c7966df8
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-05-20 01:37:55 -0400

    team activity5

--- week5/main.js
+++ week5/main.js
@@ -1,5 +1,5 @@
-import {Hikes} from './hikes.js';
-const myHikes = new Hikes('hikeListId');
+import {hikes} from './hikes.js';
+const myHike = new Hikes('hikeListId');
 window.addEventListener("load", () => {
-    myHikes.showHikeList();
-});
+
+}); 
