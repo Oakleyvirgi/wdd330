d84caec33d883ceda18e4093cff539af738a620c
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-05-21 14:59:47 -0400

    team activity week5

--- week5/script.js
+++ week5/script.js
@@ -1,36 +1,103 @@
-const name = document.querySelector('#name')
-const password = document.querySelector('#password')
-const passwordConfirm = document.querySelector('#confirmPassword')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+export default class Hikes {
+  constructor() {
+    this.imgBasePath = "//byui-cit.github.io/cit261/examples/";
+  }
+  showHikeList(hikeList, hikeListElement) {
+    hikeListElement.innerHTML = "";
+    this.renderHikeList(hikeList, hikeListElement);
+  }
+  renderHikeList(hikes, parent) {
+    hikes.forEach((hike) => {
+      parent.appendChild(this.renderOneHike(hike));
+    });
+    this.setImageEventListener(hikes);
+  }
+  renderOneHike(hike) {
+    const item = document.createElement("li");
+    item.setAttribute('id', hike.id)
+    item.innerHTML = ` <h2>${hike.name}</h2>
+          <div class="image"><img src="${this.imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}" id=${hike.id}></div>
+          <div>
+                  <div>
+                      <h3>Distance</h3>
+                      <p>${hike.distance}</p>
+                  </div>
+                  <div>
+                      <h3>Difficulty</h3>
+                      <p>${hike.difficulty}</p>
+                  </div>
+          </div>`;
 
-form.addEventListener('submit', (e) => {
-  let messages = []
-  if (name.value === '' || name.value == null) {
-    messages.push('Name is required')
+    return item;
   }
 
-  if (password.value.length <= 6) {
-    messages.push('Password must be longer than 6 characters')
+
+  setImageEventListener(hikes) {
+    hikes.forEach((hike) => {
+      //document.getElementById("mybtn").addEventListener("click", () {
+      document.getElementById(hike.id).addEventListener("click", event => {
+        event.preventDefault();
+    
+
+        const fullDetails = document.getElementById(hike.id);
+
+        fullDetails.innerHTML = ` <h2>${hike.name}</h2>
+                  <div>
+                      <h3>Distance</h3>
+                      <p>${hike.distance}</p>
+                  </div>
+                  <div>
+                      <h3>Difficulty</h3>
+                      <p>${hike.difficulty}</p>
+                  </div>
+                  <h3>Description</h3>
+              <p>${hike.description}</p><br>
+           <h3>Directions</h3>
+               <p>${hike.directions}</p><br> 
+               <button id="mybtn${hike.id}">Go Back</button>
+              `;
+
+
+        document.getElementById("mybtn" + hike.id).addEventListener("click", event => {
+          event.stopPropagation();
+
+            const backDetails = document.getElementById(hike.id);
+            backDetails.innerHTML = ` <h2>${hike.name}</h2>
+            <div class="image"><img src="${this.imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}" id=${hike.id}></div>
+            <div>
+                    <div>
+                        <h3>Distance</h3>
+                        <p>${hike.distance}</p>
+                    </div>
+                    <div>
+                        <h3>Difficulty</h3>
+                        <p>${hike.difficulty}</p>
+                    </div>
+            </div>`;
+          });
+        
+      });
+
+    });
   }
 
-  if (password.value.length >= 20) {
-    messages.push('Password must be less than 20 characters')
-  }
 
-  if (password.value === 'password' ) {
-    messages.push('Password cannot be password')
-  }
-  if (password.value === '' || password.value == null) {
-      messages.push('Password must not be blank or null')
-  }
+  renderFullDetails(hike) {
+    const fullDetails = document.getElementById("click");
 
-  if (password.value != passwordConfirm.value) {
-      messages.push('Password does not match confirmation')
-  }
+    fullDetails.innerHTML = ` <h2>${hike.name}</h2>
+          <div class="image"><img src="${this.imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}" id=${hike.id}></div>
+          <div>
+                  <div>
+                      <h3>Distance</h3>
+                      <p>${hike.distance}</p>
+                  </div>
+                  <div>
+                      <h3>Difficulty</h3>
+                      <p>${hike.difficulty}</p>
+                  </div>
+          </div>`;
 
-  if (messages.length > 0) {
-    e.preventDefault()
-    errorElement.innerText = messages.join(', ')
+    fullDetails.classList.remove("nodisplay");
   }
-})
+};
