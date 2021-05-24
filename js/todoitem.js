17b50b95c5ed34ad890aaa4ba69d90e9a62e7ad6
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-05-24 19:55:34 -0400

    todo midterm

---
+++ js/todoitem.js
@@ -0,0 +1,23 @@
+export default class TODOItem {
+    constructor(){
+        this._id = null;
+        this._item = null;
+    }
+
+    getId() {
+        return this._id;
+    }
+
+    setId() {
+        this._id = id;
+    }
+
+    getItem() {
+        return this._item;
+    }
+
+    setItem(item) {
+        this._item = item;
+    }
+        
+}
