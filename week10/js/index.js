8f37b400a3f1f98816ff7b7c3026be20532a30c8
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-06-30 18:17:08 -0400

    Quakes showing

--- week10/js/index.js
+++ week10/js/index.js
@@ -1,27 +1,40 @@
-import { getJSON, getLocation } from './utilities.js';
-import QuakesController from './QuakesController.js';
+// import {getJSON, getLocation} from "./utilities.js";
 
-const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson' +
-`&starttime=2019-01-01` + `&endtime=2019-03-02` + `&latitude=43.814540699999995` + `&longitude=-111.78491029999999` + `maxradiuskm=100`
+import QuakesController from "./QuakesController.js";
 
-console.log(baseUrl);
+const myController = new QuakesController();
 
-getLocation().then((position) => {
-    const longitude = position.coords.longitude;  
-    const latitude = position.coords.latitude;  
-  
-    console.log(latitude, longitude);  
-  
-    const adjustUrl =  
-      baseUrl +  
-      "&latitude=" +  
-      latitude +  
-      "&longitude=" +  
-      longitude +  
-      "&maxradiuskm=100";  
-  
-    getJSON(adjustUrl);
-  
-  });
+myController.init();
 
-const shake = new QuakesController();
+const myForm = document.querySelector("#radiusSettings");
+
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let newRadius = parseInt(document.getElementById("newRadius").value);
+  myController.getQuakesByRadius(newRadius);
+});
+
+// console.log(myController);
+
+// const baseUrl = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02`;
+
+// const currentLocation = getLocation();
+
+// getLocation().then((position) => {
+//   const longitude = position.coords.longitude;
+
+//   const latitude = position.coords.latitude;
+
+//   console.log(latitude, longitude);
+
+//   const adjustUrl =
+//     baseUrl +
+//     "&latitude=" +
+//     latitude +
+//     "&longitude=" +
+//     longitude +
+//     "&maxradiuskm=100";
+
+//   const myResult = getJSON(adjustUrl);
+//   console.log(myResult);
+// });
